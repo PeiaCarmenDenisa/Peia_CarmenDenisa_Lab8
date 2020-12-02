2f6433a1bf1b4859eda94d02e678e0b2f47295a0
CARMEN-DENISA PEIA <carmen.peia@stud.ubbcluj.ro>
2020-12-02 17:31:22 +0200

    Modicare lab 9

---
+++ Models/AssignedCategoryData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Peia_CarmenDenisa_Lab8.Models
+{
+    public class AssignedCategoryData
+    {
+       
+
+            public int CategoryID { get; set; }
+            public string Name { get; set; }
+            public bool Assigned { get; set; }
+        }
+
+    }
+
